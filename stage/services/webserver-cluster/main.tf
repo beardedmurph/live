1b3ad13e845fa757b37861b58233513c72b47970
beardedmurph <darrenkmmurphy@gmail.com>
2020-08-28 21:48:14 +0100

    Second commit of modules repo

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,8 +1,21 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state20200826a"
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    region         = "eu-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/beardedmurph/modules//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name = "webservers-stage"
   instance_type = "t2.micro"
