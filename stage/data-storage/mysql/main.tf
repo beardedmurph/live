75263979d27ea303fee16257ae829f83e1f338c2
beardedmurph <darrenkmmurphy@gmail.com>
2020-08-28 16:36:22 +0100

    Initial commit of modules repo

---
+++ stage/data-storage/mysql/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = "example_database"
+  username            = "admin"
+  password            = "password"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state20200826a"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "eu-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+output "address" {
+  value       = aws_db_instance.example.address
+  description = "Connect to the database at this endpoint"
+}
+
+output "port" {
+  value       = aws_db_instance.example.port
+  description = "The port the database is listening on"
+}
---
+++ stage/services/webserver-cluster/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name = "webservers-stage"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
+}
